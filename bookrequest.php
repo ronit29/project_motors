d1ec3cdde5dbe439db18f3803799411bfd7096f3
ronit <ronitpcl@gmail.com>
2018-02-14 01:03:46 +0530

    preprod release

--- bookrequest.php
+++ bookrequest.php
@@ -1,32 +1,34 @@
 <?php
-
+require(__DIR__."/mailer/PHPMailerAutoload.php");
 $request = file_get_contents("php://input");
-$POST_data = json_decode($request);
+$POST_data = json_decode($request,true);
+// print_r($POST_data); die;
+$admin_numbers = array(9896164444,9953929476);
+$numbers = implode(',', $admin_numbers);
+$admin_sms = "New Booking From \n" .$POST_data['name']. "\nPhone: ".$POST_data['phone'];
+$user_sms  = "Hi ".$POST_data['name']."\nWe have Recieved your Query for ".$POST_data['model']."\nOur Executive will get back to you Shortly.";
 
-$admin_sms = "New Booking From " .$POST_data['name']. ' Phone: '.$POST_data['phone'];
-$user_sms  = "Hi ".$POST_data['name']." We have Recieved your Query.Our Executive will get back to you in 24 Hours.";
 
-$admin_mail = "New Booking Notification<br> " .$POST_data;
+$admin_mail = "New Booking Notification<br> ";
+foreach ($POST_data as $key => $value) {
+    $admin_mail .= $key .' -- '.$value; 
+}
 $user_mail = "";
 
-function send_sms($POST_data = NULL)
+function send_sms($message = NULL, $number = NULL)
 {
 	$apiKey = '197727AG6ZZhJh5e5a8042de';
-	$numbers = array(9896164444);
-	// $message = rawurlencode($POST_data);
-	$message = rawurlencode("HI Ronit \n Your booking has been confirmed.");
-	$numbers = implode(',', $numbers);
+	$message = rawurlencode($message);
+	// $message = rawurlencode("HI Ronit \n Your booking has been confirmed.");
     $ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, "http://my.msgwow.com/api/sendhttp.php?authkey=".$apiKey."&mobiles=919896164444&message=".$message."&sender=INMOTR&route=1&country=91"); 
+    curl_setopt($ch, CURLOPT_URL, "http://my.msgwow.com/api/sendhttp.php?authkey=".$apiKey."&mobiles=".$number."&message=".$message."&sender=INMOTR&route=1&country=91"); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     $output = curl_exec($ch); 
     curl_close($ch);      
-
-	echo $output;
 }
 
 
-function send_mail_admin($post_data = NULL)
+function send_mail($post_data = NULL)
 {
     $body = $post_data;
     $mail = new PHPMailer;
@@ -52,6 +54,9 @@ function send_mail_admin($post_data = NULL)
     return 'error';
 }  
 
-send_sms($admin_sms);
+send_sms($admin_sms,$numbers);
+send_sms($user_sms,$POST_data['phone']);
+send_mail($admin_mail);
+print "success";
 
 ?>
--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 var app = angular.module("myApp", []);
 
 app.controller('booking', function($scope ,$http) {
-
+    $scope.booksuccess = false;
+    $scope.book_load = false;
+    $scope.read_only = false;
 	$scope.book_service = function(input){
+		$scope.book_load = true;
+	    $scope.read_only = true;
 		$http({
            method: 'POST',
            url: "bookrequest.php",
            data:input,
            }).then(function(result) {
+   	  		  $scope.book_load = false;
+	  		  $scope.read_only = false;
+           	  $scope.booksuccess = true;
             }, function(error) {
+			  $scope.book_load = false;
+		      $scope.read_only = false;
+
        });
 	}
 });
