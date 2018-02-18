41318d502fea56a9e971522e74c10c4a330f1cca
ronit <ronitpcl@gmail.com>
2018-02-19 03:25:30 +0530

    sitemap

--- bookrequest.php
+++ bookrequest.php
@@ -1,62 +1,73 @@
 <?php
+
 require(__DIR__."/mailer/PHPMailerAutoload.php");
 $request = file_get_contents("php://input");
+if (!$request && empty($request)) {
+header('Location: /'); 
+} 
+
+
 $POST_data = json_decode($request,true);
-// print_r($POST_data); die;
 $admin_numbers = array(9896164444,9953929476);
 $numbers = implode(',', $admin_numbers);
 $admin_sms = "New Booking From \n" .$POST_data['name']. "\nPhone: ".$POST_data['phone'];
 $user_sms  = "Hi ".$POST_data['name']."\nWe have Recieved your Query for ".$POST_data['model']."\nOur Executive will get back to you Shortly.";
 
 
-$admin_mail = "New Booking Notification<br> ";
+$admin_mail = array();
+$admin_mail['Info'] = "<h2>New Booking Notification</h2><br><br><ul> ";
 foreach ($POST_data as $key => $value) {
-    $admin_mail .= $key .' -- '.$value; 
+
+    $admin_mail['Info'] .= '<li>'.$key .' -- '.$value.'</li>'; 
 }
+$admin_mail['Info'] .= "</ul>";
 $user_mail = "";
+
+
+$status  = array();
+
 
 function send_sms($message = NULL, $number = NULL)
 {
 	$apiKey = '197727AG6ZZhJh5e5a8042de';
 	$message = rawurlencode($message);
-	// $message = rawurlencode("HI Ronit \n Your booking has been confirmed.");
     $ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, "http://my.msgwow.com/api/sendhttp.php?authkey=".$apiKey."&mobiles=".$number."&message=".$message."&sender=INMOTR&route=1&country=91"); 
+    curl_setopt($ch, CURLOPT_URL, "http://my.msgwow.com/api/sendhttp.php?authkey=".$apiKey."&mobiles=".$number."&message=".$message."&sender=INMOTR&route=1&country=91&response=json"); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     $output = curl_exec($ch); 
-    curl_close($ch);      
+    curl_close($ch);     
+    return $output; 
 }
 
 
-function send_mail($post_data = NULL)
+function send_mail($body =NULL,$flag = NULL)
 {
-    $body = $post_data;
     $mail = new PHPMailer;
     $mail->isSMTP();                                
-    $mail->Host = 'email-smtp.us-east-1.amazonaws.com';
+    $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;                            
-    $mail->Username = 'AKIAITLTAUVPICLE2K4Q';              
-    $mail->Password = 'AozIDFFnSPZTxbWI/yBLLoaO4fCX7+7T4Vl6whsvtKaD';                        
+    $mail->Username = 'indianmotorsggn@gmail.com';              
+    $mail->Password = 'airport123';                        
     $mail->SMTPSecure = 'tls';                         
-    $mail->Port = 25;    
-
-    $mail->setFrom('indianMotors@gmail.com', 'IndianMotors');
+    $mail->Port = 587;    
+    $mail->setFrom('indianMotorsggn@gmail.com', 'IndianMotors');
     $mail->FromName = 'IndianMotors';
-    $mail->addAddress('ronit.dhingra@ticketmaster.com', 'Ronit Dhingra');   
-    $mail->addCC('ronitpcl@gmail.com');
-    // $mail->addBCC('bcc@example.com'); 
+    $mail->addAddress('ronitdhingra@live.com', 'Ronit Dhingra');   
+    $mail->addCC('ravikumarparjapat081@gmail.com');
+    $mail->addBCC('indianMotorsggn@gmail.com'); 
     $mail->isHTML(true); 
-    $mail->Subject = 'New Service Booking';
+    $mail->Subject = 'Booking Confirmed';
     $mail->Body = $body;
     if(!$mail->send()) {
-    	return;
+       $status[$flag] = 'Mail not sent';
     } 
-    return 'error';
+    $status[$flag] = 'Mail Sent';
 }  
 
-send_sms($admin_sms,$numbers);
-send_sms($user_sms,$POST_data['phone']);
-send_mail($admin_mail);
-print "success";
+$status['admin_sms'] = send_sms($admin_sms,$numbers);
+$status['user_sms'] =  send_sms($user_sms,$POST_data['phone']);
+send_mail($admin_mail['Info'],'admin_mail');
+$response['status'] =  (json_decode($status['user_sms'],true)['type'] == 'success') ? "success" : 'failure';
+echo json_encode($response);
 
 ?>
--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", [ 'ngRoute']);
+
+
+
+
+
 
 app.controller('booking', function($scope ,$http) {
+  
     $scope.booksuccess = false;
     $scope.book_load = false;
     $scope.read_only = false;
 	$scope.book_service = function(input){
 		$scope.book_load = true;
 	    $scope.read_only = true;
-		$http({
+  		$http({
            method: 'POST',
            url: "bookrequest.php",
            data:input,
            }).then(function(result) {
-   	  		  $scope.book_load = false;
-	  		  $scope.read_only = false;
+   	  	   	  $scope.book_load = false;
+	  		      $scope.read_only = false;
            	  $scope.booksuccess = true;
             }, function(error) {
 			  $scope.book_load = false;
